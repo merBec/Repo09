9138d8e5436fc987d21c7d3a88e3e051e359be1c
merBec <becic.merjema@gmail.com>
2021-09-21 20:35:58 +0200

    Added ex01-ex16

---
+++ ex13/src/index.js
@@ -0,0 +1,14 @@
+function inverseWhile() {
+  var fiveNumbers = "";
+  var i = 5;
+  while (i >= 0) {
+    fiveNumbers += i;
+    if (i !== 0) {
+      fiveNumbers += ",";
+    }
+    i--;
+  }
+  return fiveNumbers;
+}
+console.log(inverseWhile());
+module.exports = inverseWhile;
---
+++ ex15/src/index.js
@@ -0,0 +1,16 @@
+function myDoWhile() {
+  var myNumbers = "";
+  var i = 0;
+  do {
+    myNumbers += i;
+    if (i !== 9) {
+      myNumbers += ", ";
+    }
+    i++;
+  } while (i < 10);
+
+  return myNumbers;
+}
+myDoWhile();
+console.log(myDoWhile());
+module.exports = myDoWhile;
